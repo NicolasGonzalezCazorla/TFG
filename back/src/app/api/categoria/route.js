fc3dfc41e64e5a45987f86c64f5cba002d13532b
Nicolas Gonzalez Cazorla <ngoncaz2912@g.educaand.es>
2026-04-08 09:00:28 +0200

    Direccion de la api corregida

--- back/src/app/api/categoria/route.js
+++ back/src/app/api/categoria/route.js
@@ -1,13 +1,11 @@
 import { supabase } from "@/app/lib/supabase";
 
 export async function GET() {
-
-    const {data, error} = await supabase.from("citas").select("*");
+    const {data, error} = await supabase.from("categoria").select("*");
 
     if (error){
         return new Response(JSON.stringify({ error: error.message }), { status: 500 });
     }
-    
-    return new Response(JSON.stringify(data), { status: 200 });
-}
 
+    return new Response(JSON.stringify({ data }), { status: 200 });
+}
